af9c03b8f64a72917f374c9b1a0c6a5e574c616b
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2023-08-21 21:55:42 -0400

    Completed task 15

--- Program.cs
+++ Program.cs
@@ -15,7 +15,21 @@ Console.WriteLine("\nTask 13");
 // числа или сообщает, что третьей цифры нет.
 // 645 -> 5     78 -> there is not thrid digit     32679 -> 6
 
-Console.WriteLine("Type a number: ");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Type a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-  
+// if(num>=100) ConsoleWriteLine()
+
+
+
+Console.WriteLine("\nTask 15");
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+// 6 -> Yes     7 -> Yes      1 -> No
+
+Console.WriteLine("Type a number: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if ((day>0)&&(day<6)) Console.WriteLine("No");
+else if ((day>5)&&(day<8)) Console.WriteLine("Yes");
+else Console.WriteLine("There is no such day");
